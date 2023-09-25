2bda2254fde06feaf22d152d3959d6e07b1aead9
kiraberser <121148745+kiraberser@users.noreply.github.com>
2023-09-25 17:57:05 -0600

    Caesar Cipher Part1

---
+++ caesarChiper.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text_letter, shift_amount):
+    cipher_text = ""
+    for letter in text_letter:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_message = alphabet[new_position]
+        cipher_text += alphabet[new_message]
+    print(f"The encode text is {cipher_text}")
+
+
+encrypt(text_letter=text, shift_amount=shift)
+
