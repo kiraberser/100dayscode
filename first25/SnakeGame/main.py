0f473d416b6c04ecfc1c77090ef0c68c2dcddac5
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-08-07 13:16:13 -0600

    Day 22 PingPong

--- first25/SnakeGame/main.py
+++ first25/SnakeGame/main.py
@@ -15,10 +15,10 @@ food = Food()
 score_count = Score()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+screen.onkey(snake.left, "a")
 
 game_is_on = True
 while game_is_on:
