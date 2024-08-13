31c63132eebd3fec041ae82a061d4170dfc878eb
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-08-13 00:03:24 -0600

    Read File and Actualization of high score

--- first25/SnakeGame/scoreboard.py
+++ first25/SnakeGame/scoreboard.py
@@ -5,7 +5,8 @@ class Score(Turtle):
         super().__init__()
         self.color("white")
         self.hideturtle()
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.penup()
         self.goto(0, 260)
@@ -22,6 +23,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
         
