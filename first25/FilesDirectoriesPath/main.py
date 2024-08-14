3d7b3eee7677732afff75d04e96eaa2c01f0a676
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-08-13 22:44:07 -0600

    DAY 24 Mail Merge Project

--- first25/FilesDirectoriesPath/main.py
+++ first25/FilesDirectoriesPath/main.py
@@ -1,5 +1,6 @@
-with open('my_file.txt', mode="a") as file:
-    file.write("\nNew text.")
+#with open("/Users/Edwin Vega/Desktop/my_file.txt") as file:
+#    contents = file.read()
+#    print(contents)
 
-with open('new_file.txt', mode="w") as file:
-    file.write("New Text.")
+#with open('new_file.txt', mode="w") as file:
+#    file.write("New Text.")
