11342b60753be110842ff3f5496e4b1a4663bc0a
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-08-29 03:09:53 -0600

    Day 34 Api Authetication and Enviroment Variables

---
+++ second25/day-34/main.py
@@ -0,0 +1,37 @@
+import requests
+from twilio.rest import Client
+import os
+
+LAT = 19.173773
+LNG = -96.134224
+
+account_sid = os.environ.get("TWILIO_ACCOUNT")
+auth_token = os.environ.get("TWILIO_API_KEY")
+
+parameters = {
+    "lat": LAT,
+    "lon": LNG,
+    "appid": os.environ.get("OPW_API_KEY"),
+    "cnt": 4
+}
+
+response = requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+#id = data["list"][0]['weather'][0]['id']
+
+will_rain = False
+for hour_data in data["list"]:
+    condition_code = hour_data["weather"][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Parece que va a llover hoy, llevate un paraguas o al menos una chamarra",
+        from_="+18178359131",
+        to=""
+    )
+
