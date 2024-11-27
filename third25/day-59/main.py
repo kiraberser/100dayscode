e481f76cac45210dc44d5460e8666c5f0e782cad
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-11-27 03:51:58 -0600

    Sending email messages with smtp

--- third25/day-59/main.py
+++ third25/day-59/main.py
@@ -1,35 +1,40 @@
 from flask import Flask, render_template, request
 
-#Peticiones
 from post import Post
+from send_message import Send_Message
 
 app = Flask(__name__)
-all_post = Post()
+posts = Post()
+send = Send_Message()
 
 #Ruta home
 @app.route('/')
 def home():
-    return render_template('index.html', posts=all_post.data_blog)
+    return render_template('index.html', posts=posts.data_blog)
 
 #Ruta post
 @app.route('/post/<int:id>')
 def post(id):
-    if 0 < id <= len(all_post.data_blog):
-        all_post.get_post(id)
-        return render_template('post.html', title=all_post.title, subtitle=all_post.subtitle, body=all_post.body, image_url=all_post.image)
+    if 0 < id <= len(posts.data_blog):
+        posts.get_post(id)
+        return render_template('post.html', title=posts.title, subtitle=posts.subtitle, body=posts.body, image_url=posts.image)
     return render_template('error.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        name = request.form['first-name']
+        email= request.form['email']
+        tel = request.form['phone-number']
+        message = request.form['message']
+        if name and email:
+            send.send_email(email=email, name=name, cel=tel, message=message)
+            return render_template('contact.html', message_sended='Succesfuly sent your message')
     return render_template('contact.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/form')
-def form():
-    return render_template('login.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def receive_data():
@@ -37,8 +42,8 @@ def receive_data():
         username = request.form['username']
         password = request.form['password']
         if username and password:
-            return render_template('index.html', user=username, posts=all_post.data_blog)
-    return render_template('login.html', user=username, posts=all_post.data_blog)
+            return render_template('index.html', user=username, posts=posts.data_blog)
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
