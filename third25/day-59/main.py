5fca2b9e24b52ee595992ed8e2cdb2a9b5ab6ec6
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-11-27 01:43:04 -0600

    Adding Log-in Flask

--- third25/day-59/main.py
+++ third25/day-59/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Peticiones
 from post import Post
@@ -14,11 +14,10 @@ def home():
 #Ruta post
 @app.route('/post/<int:id>')
 def post(id):
-    if id <= len(all_post.data_blog):
+    if 0 < id <= len(all_post.data_blog):
         all_post.get_post(id)
         return render_template('post.html', title=all_post.title, subtitle=all_post.subtitle, body=all_post.body, image_url=all_post.image)
-    else:
-        return render_template('error.html')
+    return render_template('error.html')
 
 @app.route('/contact')
 def contact():
@@ -27,6 +26,19 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form')
+def form():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def receive_data():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username and password:
+            return render_template('index.html', user=username, posts=all_post.data_blog)
+    return render_template('login.html', user=username, posts=all_post.data_blog)
 
 if __name__ == "__main__":
     app.run(debug=True)
