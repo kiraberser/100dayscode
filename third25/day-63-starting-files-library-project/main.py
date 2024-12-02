3f01a8f862a116398588795c5e2b1df25cb40183
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-12-01 20:11:08 -0600

    Connecting SQLAlchemy

--- third25/day-63-starting-files-library-project/main.py
+++ third25/day-63-starting-files-library-project/main.py
@@ -1,51 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer,  String, Float
 import sqlite3
+from flask_migrate import Migrate
+
+db = sqlite3.connect("new-books-collection.db")
 
 app = Flask(__name__)
-db = sqlite3.connect("books-collection.db", check_same_thread=False)
-cursor = db.cursor()
-#database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+migrate = Migrate(app, db)
 
-""" def create_database():    
-    try:
-        cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-    except sqlite3.OperationalError as e:
-        return print('Table exist')
-create_database()  """
+class Base(DeclarativeBase):
+    pass
 
-def insert_database(pk, title, author, rating):
-    try:
-        cursor.execute(f"INSERT INTO books (id, title, author, rating) VALUES (?, ?, ?, ?)", (pk, title, author, rating))
-        db.commit()
-    except Exception as e:
-        return str(e)
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
-all_books = []
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250),unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    
+    def __repr__(self):
+        return f'<Book {self.title}>'
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    return render_template('index.html', books=Book.query.all())
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
-    pk = len(all_books) + 1
     if request.method == 'POST':
-        all_books.append(
-            {
-                'id': pk,
-                'title': request.form.get('title'),
-                'author': request.form.get('author'),
-                'rating': float(request.form.get('rating'))
-            }
-        )
-        insert_database(
-            pk=pk,
+        new_book = Book(
             title=request.form.get('title'),
             author=request.form.get('author'),
-            rating=float(request.form.get('rating'))
+            rating=request.form.get('rating')
         )
+        db.session.add(new_book)
+        db.session.commit()
+        print(Book.query.all())
         return redirect(url_for('home'))
     return render_template('add.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
