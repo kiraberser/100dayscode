cd95fbe463bcf2d79e1d0358479c3a813a414ea7
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-12-01 22:31:41 -0600

    CRUD operator and Styles

--- third25/day-63-starting-files-library-project/main.py
+++ third25/day-63-starting-files-library-project/main.py
@@ -5,18 +5,15 @@ from sqlalchemy import Integer,  String, Float
 import sqlite3
 from flask_migrate import Migrate
 
-db = sqlite3.connect("new-books-collection.db")
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
-migrate = Migrate(app, db)
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -25,11 +22,8 @@ class Book(db.Model):
     rating: Mapped[float] = mapped_column(Float, nullable=False)
     
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return f'<Book {self.title}>'    
     
-with app.app_context():
-    db.create_all()
-
 @app.route('/')
 def home():
     return render_template('index.html', books=Book.query.all())
@@ -44,11 +38,56 @@ def add():
         )
         db.session.add(new_book)
         db.session.commit()
-        print(Book.query.all())
         return redirect(url_for('home'))
     return render_template('add.html')
 
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    book_id = request.args.get('id')
+    if request.method == 'GET':
+        book_to_update = Book.query.get(book_id)
+        return render_template('edit.html', book=book_to_update)    
+    
+    if request.method == 'POST':
+        with app.app_context():
+            book_to_update = db.get_or_404(Book, book_id)  
+            book_to_update.rating = request.form['rating']
+            db.session.commit()
+        return redirect(url_for('home'))
+
+@app.route('/delete/<int:book_id>', methods=['GET'])
+def delete(book_id):
+    with app.app_context():
+        book_to_delete = db.get_or_404(Book, book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+# CRUD WITH FLASK
+
+#with app.app_context():
+#    #All
+#    all_books = db.session.execute(db.select(Book).order_by(Book.title)).scalars()
+#    
+#    #Read by query
+#    book = db.session.execute(db.select(Book).where(Book.title == 'Habitos Atomicos')).scalar()
+#    
+#    #Update by query
+#    book_to_update = db.session.execute(db.select(Book).where(Book.title == 'Habitos Atomicos')).scalar()
+#    book_to_update.title = "Atomic Habits"
+#    
+#    #Update by Primary Key
+#    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#    # or book_to_update = db.get_or_404(Book, book_id)  
+#    book_to_update.title = "Harry Potter and the Goblet of Fire"
+#    db.session.commit()  
+#
+#    #Delete by Primary Key
+#    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#    # or book_to_delete = db.get_or_404(Book, book_id)
+#    db.session.delete(book_to_delete)
+#    db.session.commit()
